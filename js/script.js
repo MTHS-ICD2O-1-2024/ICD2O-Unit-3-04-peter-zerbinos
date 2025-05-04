2d428e08600590d42f06823d46d225484f4d79ee
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-04 23:15:58 +0000

    added index.html and script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Peter Zerbinos All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculateFahrenheitToCelsius() {
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit').value)
+
+  const celsius = (fahrenheit - 32) * (5 / 9)
+
+  document.getElementById('celsius').innerHTML = fahrenheit + '°F when converted to celsius is ' + celsius.toFixed(3) + '°C'
+}
